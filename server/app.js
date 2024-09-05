388ae5b5acfaa78f6f7a846ebe15e20380abd121
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-09-06 00:03:52 +0530

    First commit

--- server/app.js
+++ server/app.js
@@ -3,36 +3,22 @@ require('dotenv').config(); // Import dotenv to load environment variables
 const express = require('express');
 const app = express();
 const cors=require('cors');
-app.use(cors());
-
+app.use(cors())
 
 // Load Zego credentials from environment variables
 const appId = process.env.ZEGO_APP_ID;
 const serverSecret = process.env.ZEGO_SERVER_SECRET;
 
-function randomID(len) {
-    let result = '';
-    if (result) return result;
-    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-      maxPos = chars.length,
-      i;
-    len = len || 5;
-    for (i = 0; i < len; i++) {
-      result += chars.charAt(Math.floor(Math.random() * maxPos));
-    }
-    return result;
-  }
 app.get('/generate-token', (req, res) => {
-    
   // Generate the kitToken using the Zego appId and serverSecret in the backend
   const roomId = req.query.roomId;
-  console.log(roomId)
+  const userId = req.query.userId;
 
   const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
     appId,
     serverSecret,
     roomId,
-    randomID(5)
+    userId
   );
 
   res.json({ kitToken });
--- src/Room.js
+++ src/Room.js
@@ -3,21 +3,30 @@ import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import { useParams } from 'react-router-dom';
 
 
-
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
 
 
 const Room=()=> {
       const {roomId} =useParams();
       let myMeeting = async (element) => {
      // generate Kit Token
-     const response = await fetch(
-        `https://video-calling-mul.onrender.com/generate-token?roomId=${roomId}`
-      );
+     const gh3t2dlskgHd123 = process.env.REACT_gh3t2dlskgHd123;
+     const gsh$2mpo9362 = process.env.REACT_gsh$2mpo9362;
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(gh3t2dlskgHd123, gsh$2mpo9362, roomId,  randomID(5),  randomID(5));
 
-      const kittoken = await response.json();
-       console.log("kit token",kittoken);
      // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kittoken);
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
       // start the call
       zp.joinRoom({
         container: element,
